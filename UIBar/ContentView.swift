7e6151758250beedaf1cfa1169059299117a562e
Renan Dias <renan@jera.com.br>
2019-09-08 21:13:02 -0400

    Initial Commit

---
+++ UIBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIBar
+//
+//  Created by Renan Benatti Dias on 08/09/19.
+//  Copyright © 2019 Renan Benatti Dias. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
